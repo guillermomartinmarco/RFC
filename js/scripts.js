b2d9a55102f2363dd6e823abcad5c2cb1ae20253
Guillermo Martín Marco <guillermomartin.marco@gmail.com>
2023-08-13 04:07:06 -0300

    modificacion ./css/style.css y ./js/scripts.js para soporte de cambio de estilo de links

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 function langChange(el) {
   document.body.setAttribute('lang', el.value);
 }
+
+function linkStyleChange(style) {
+  switch (style.value) {
+    case 'no-style':
+      $('a:link').css('color', 'unset');
+      $('a:link').css('text-decoration', 'none');
+      break;
+    case 'normal':
+      $('a:link').css('color', 'Blue');
+      $('a:link').css('text-decoration', 'underline');
+      break;
+    default:
+      break;
+  }
+
+}
